58ada861c093b718c9d62838993b128c0594f018
Hakim Lim <hakimlim1990@gmail.com>
2022-07-07 21:38:32 +0800

    create folder structure created models for household and members install packages express boiler plate

---
+++ src/models/houseModel.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose'
+import { IMemberDetails } from './memberModel'
+
+export enum HousingType {
+    LANDED = "LANDED",
+    CONDOMINIUM = "CONDOMINIUM",
+    HDB = "HDB"
+}
+
+export interface IHouse {
+    id: string
+    housingType: HousingType
+    familyMembers: IMemberDetails[]
+}
---
+++ src/models/memberModel.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose'
+
+
+export enum OccupationType {
+    UNEMPLOYED = "UNEMPLOYED",
+    STUDENT = "STUDENT",
+    EMPLOYED = "EMPLOYED"
+}
+
+export interface IMemberDetails {
+    id: string
+    houseId: string
+    name: string
+    gender: string
+    spouse: string
+    occupationType: OccupationType
+    annualIncome: number
+    DOB: Date
+}
+
+
