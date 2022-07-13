acef1bb902a19defd9ff766b52f47c4a34ae7f03
Hakim Lim <hakimlim1990@gmail.com>
2022-07-13 20:49:55 +0800

    update elder bonus grant to HDB only implemented YOLO GST grant logic

--- src/controllers/grantController.ts
+++ src/controllers/grantController.ts
@@ -22,185 +22,246 @@ import { householdValidator } from '../resources/householdValidation'
  */
 export class GrantController {
   private mongoLookup = {
-    '$lookup': {
-      'from': 'members', 
-      'localField': 'familyMembers', 
-      'foreignField': '_id', 
-      'as': 'familyMembers', 
-      'pipeline': [
+    $lookup: {
+      from: 'members',
+      localField: 'familyMembers',
+      foreignField: '_id',
+      as: 'familyMembers',
+      pipeline: [
         {
-          '$set': {
-            'born': {
-              '$dateFromString': {
-                'dateString': '$DOB', 
-                'format': '%d/%m/%Y'
-              }
+          $set: {
+            born: {
+              $dateFromString: {
+                dateString: '$DOB',
+                format: '%d/%m/%Y',
+              },
             },
-            '_id' : { '$toString' : '$_id'},
-          }
-        }, {
-          '$set': {
-            'age': {
-              '$divide': [
+            _id: { $toString: '$_id' },
+          },
+        },
+        {
+          $set: {
+            age: {
+              $divide: [
                 {
-                  '$subtract': [
-                    '$$NOW', '$born'
-                  ]
-                }, 31536000000
-              ]
-            }
-          }
-        }
-      ]
-    }
+                  $subtract: ['$$NOW', '$born'],
+                },
+                31536000000,
+              ],
+            },
+          },
+        },
+      ],
+    },
   }
+
+  private mongoSetTotalIncome = {
+    $set: {
+      totalIncome: {
+        $sum: '$familyMembers.annualIncome',
+      },
+    },
+  }
+
+  /**
+   * Household income < 150k
+   * Age < 16
+   */
 
   @Get('/grant/student-encouragment-bonus')
   async getStudentEncouragementBonus() {
-    const response = await HouseholdModel.aggregate(
-      [
-        this.mongoLookup, {
-          '$set': {
-            'totalIncome': {
-              '$sum': '$familyMembers.annualIncome'
-            }
-          }
-        }, {
-          '$match': {
-            '$and': [
-              {
-                'totalIncome': {
-                  '$lt': 150000
-                }
-              }, {
-                'familyMembers.age': {
-                  '$lt': 16
-                }
-              }
-            ]
-          }
-        }, {
-          '$set': {
-            '_id' : { '$toString' : '$_id'},
-            'familyMembers': {
-              '$filter': {
-                'input': '$familyMembers', 
-                'as': 'member', 
-                'cond': {
-                  '$lt': [
-                    '$$member.age', 16
-                  ]
-                }
-              }
-            }
-          }
-        }
-      ]
-    )
-    return response
-  }
-
-  @Get('/grant/family-togetherness-scheme')
-  async getFamilyTogethernessScheme() {
-    const response = HouseholdModel.aggregate([
-      this.mongoLookup, {
-        '$match': {
-          '$and': [
+    const response = await HouseholdModel.aggregate([
+      this.mongoLookup,
+      this.mongoSetTotalIncome,
+      {
+        $match: {
+          $and: [
             {
-              'familyMembers.maritalStatus': 'MARRIED'
-            }, {
+              totalIncome: {
+                $lt: 150000,
+              },
+            },
+            {
               'familyMembers.age': {
-                '$lt': 18
-              }
-            }
-          ]
-        }
-      }, {
-        '$set': {
-          '_id' : { '$toString' : '$_id'}, 
-          'familyMembers': {
-            '$filter': {
-              'input': '$familyMembers', 
-              'as': 'member', 
-              'cond': {
-                '$lt': [
-                  '$$member.age', 18
-                ]
-              }
-            }
-          }
-        }
-      }
+                $lt: 16,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $set: {
+          _id: { $toString: '$_id' },
+          familyMembers: {
+            $filter: {
+              input: '$familyMembers',
+              as: 'member',
+              cond: {
+                $lt: ['$$member.age', 16],
+              },
+            },
+          },
+        },
+      },
     ])
     return response
   }
 
-  @Get('/grant/elder-bonus')
-  async getElderBonus() {
-    const response = HouseholdModel.aggregate(
-      [
-        this.mongoLookup, {
-          '$match': {
-            'familyMembers.age': {
-              '$gt': 50
-            }
-          }
-        }, {
-          '$set': {
-            '_id': {
-              '$toString': '$_id'
-            }, 
-            'familyMembers': {
-              '$filter': {
-                'input': '$familyMembers', 
-                'as': 'member', 
-                'cond': {
-                  '$gt': [
-                    '$$member.age', 50
-                  ]
-                }
-              }
-            }
-          }
-        }
-      ]
-    )
+  /**
+   * Marital Status only MARRIED
+   * Age < 18
+   */
+
+  @Get('/grant/family-togetherness-scheme')
+  async getFamilyTogethernessScheme() {
+    const response = HouseholdModel.aggregate([
+      this.mongoLookup,
+      {
+        $match: {
+          $and: [
+            {
+              'familyMembers.maritalStatus': 'MARRIED',
+            },
+            {
+              'familyMembers.age': {
+                $lt: 18,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $set: {
+          _id: { $toString: '$_id' },
+          familyMembers: {
+            $filter: {
+              input: '$familyMembers',
+              as: 'member',
+              cond: {
+                $lt: ['$$member.age', 18],
+              },
+            },
+          },
+        },
+      },
+    ])
     return response
   }
+
+  /**
+   * Must be HDB only
+   * Age > 50
+   */
+
+  @Get('/grant/elder-bonus')
+  async getElderBonus() {
+    const response = HouseholdModel.aggregate([
+      this.mongoLookup,
+      {
+        $match: {
+          $and: [
+            {
+              'familyMembers.age': {
+                $gt: 50,
+              },
+            },
+            {
+              housingType: 'HDB',
+            },
+          ],
+        },
+      },
+      {
+        $set: {
+          _id: {
+            $toString: '$_id',
+          },
+          familyMembers: {
+            $filter: {
+              input: '$familyMembers',
+              as: 'member',
+              cond: {
+                $gt: ['$$member.age', 50],
+              },
+            },
+          },
+        },
+      },
+    ])
+    return response
+  }
+
+  /**
+   * Household with age < 5
+   */
 
   @Get('/grant/baby-sunshine-grant')
   async getBabySunshineGrant() {
-    const response = HouseholdModel.aggregate(
-      [
-        this.mongoLookup, {
-          '$match': {
-            'familyMembers.age': {
-              '$lt': 5
-            }
-          }
-        }, {
-          '$set': {
-            '_id': {
-              '$toString': '$_id'
-            }, 
-            'familyMembers': {
-              '$filter': {
-                'input': '$familyMembers', 
-                'as': 'member', 
-                'cond': {
-                  '$lt': [
-                    '$$member.age', 5
-                  ]
-                }
-              }
-            }
-          }
-        }
-      ]
-    )
+    const response = HouseholdModel.aggregate([
+      this.mongoLookup,
+      {
+        $match: {
+          'familyMembers.age': {
+            $lt: 5,
+          },
+        },
+      },
+      {
+        $set: {
+          _id: {
+            $toString: '$_id',
+          },
+          familyMembers: {
+            $filter: {
+              input: '$familyMembers',
+              as: 'member',
+              cond: {
+                $lt: ['$$member.age', 5],
+              },
+            },
+          },
+        },
+      },
+    ])
     return response
   }
 
+  /**
+   * Household Annual Income < 100k
+   * Must be HDB only
+   */
+
   @Get('/grant/yolo-gst-grant')
-  async getYoloGstGrant() {}
+  async getYoloGstGrant() {
+    const response = HouseholdModel.aggregate([
+      this.mongoLookup,
+      this.mongoSetTotalIncome,
+      {
+        $match: {
+          $and: [
+            {
+              totalIncome: { $lt: 100000 },
+            },
+            {
+              familyMembers: {
+                $exists: true,
+                $ne: [],
+              },
+            },
+            {
+              housingType: 'HDB',
+            },
+          ],
+        },
+      },
+      {
+        $set: {
+          _id: {
+            $toString: '$_id',
+          },
+        },
+      },
+    ])
+    return response
+  }
 }
